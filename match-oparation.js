0fcf936a80a0d29c3a2068993b1fa444fb12d314
jahangir-1995 <jahangir199511@gmail.com>
2022-01-26 08:56:04 +0600

    all add

---
+++ match-oparation.js
@@ -0,0 +1,17 @@
+var aluPrice = 35;
+var begunPrice = 25;
+var total = aluPrice + begunPrice;
+console.log(total);
+
+var priceDifference = aluPrice - begunPrice;
+console.log(priceDifference);
+
+// Video 06
+var age = 26;
+age = age + 1;
+console.log(age);
+// ekoi kaj onno vabe
+var myAge = 28;
+myAge += 1;
+myAge++;
+console.log(myAge);
