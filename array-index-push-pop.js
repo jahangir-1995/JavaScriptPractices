bdfe755ce50c878b57854b341ceb7366d947bcbe
jahangir-1995 <jahangir199511@gmail.com>
2022-01-27 00:19:20 +0600

    push and pop

---
+++ array-index-push-pop.js
@@ -0,0 +1,26 @@
+var friendAge2 = [20, 22, 23, 24, 25];
+
+var friendName =['manik', 'altab', 'boshir', 'kader', 'mamun'];
+
+var vowelsAll = ['a', 'e', 'i', 'o', 'u'];
+
+var findIndex = friendAge2[3]; 
+
+
+// jodi position khuje na pai tobe UNDIFINED dekhabe 
+// array er man poriborton korte chaile 
+
+friendAge2 [3] = 30 ;
+
+// console.log(findIndex);
+// console.log(friendAge2);
+
+// add korte push
+friendAge2.push(29);
+friendAge2.push(31);
+console.log(friendAge2);
+
+// remove korte pop
+friendName.pop();
+friendName.pop();
+console.log(friendName);
