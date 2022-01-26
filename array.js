fc2dd03248e2549d38b014f2c9588240c048b701
jahangir-1995 <jahangir199511@gmail.com>
2022-01-26 23:28:35 +0600

    array add

---
+++ array.js
@@ -0,0 +1,10 @@
+var friendAge = [20, 22, 23, 24, 25];
+
+var friendName =['manik', 'altab', 'boshir', 'kader', 'mamun'];
+
+var vowelsAll = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(friendAge);
+console.log(friendAge.length);
+
+// ekoi vabe sob gular ber kora jabe... 
