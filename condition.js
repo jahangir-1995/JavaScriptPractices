48d4e05c7d13374c6760a8b154cd23cf74c0ba3e
jahangir-1995 <jahangir199511@gmail.com>
2022-01-27 19:17:16 +0600

    multipleCondisionAdd

---
+++ condition.js
@@ -0,0 +1,46 @@
+// var iphonePrice = 70000;
+// var myBuject = 80000;
+
+// if (iphonePrice > myBuject) {
+//     console.log('Nokia IS Best');
+// }
+// if (iphonePrice < myBuject) {
+//     console.log('Iphone IS Best');
+// }
+
+// var gotJob = true;
+// var saveMany = 150000;
+
+
+// // gotJob saveMany dui tai sotti hote hobe..
+// if(gotJob == true && saveMany > 200000) {
+//     console.log('get Marrage');
+// }
+// else{
+//     console.log('biya nai');
+// }
+
+// var gotJob2 = true;
+// var saveMany2 = 150000;
+// var buyFlat2 = true;
+
+
+// // gotJob2 saveMany2 buyFlat2 3 tai sotti hote hobe..
+// if(gotJob2 == true && saveMany2 > 200000 && buyFlat == true) {
+//     console.log('get Marrage');
+// }
+// else{
+//     console.log('biya nai');
+// }
+
+var gotJob3 = true;
+var saveMany3 = 150000;
+
+// gotJob3 saveMany3 duiter ekta sotti hote hobe..
+
+if(gotJob3 == true || saveMany3 > 200000) {
+    console.log('get Marrage');
+}
+else{
+    console.log('biya nai');
+}
