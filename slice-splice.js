21de7b0328e8925c9a203f1013127d3f2f129206
jahangir-1995 <jahangir199511@gmail.com>
2022-01-30 15:06:33 +0600

    new splice add

--- slice-splice.js
+++ slice-splice.js
@@ -1,8 +1,15 @@
 // slice usese
-
 var letter = [1, 2, 3, 4, 5, 6, 7];
-var sndLetter = letter.splice(1, 3);
-console.log(sndLetter);
+var sndLetter = letter.slice(2, 4);
+console.log(sndLetter); //Output: 2,3; 
+
+// স্লাইস এর starting এ যে নাম্বার আছে তার থেকে পরে যে নাম্বার আছে তার থেকে বিয়োগ করে যা হয় সেই কয়টা দেখাবে
+
+
+// splice usese
+var letter2 = [1, 2, 3, 4, 5, 6, 7];
+var sndLetter2 = letter2.splice(1, 3);
+console.log(sndLetter2); //Output: 2,3,4; এক বাদ যাবে । ৩ টা থাকবে 
 
 // function diye 
 function multiplyNums (num1, nums2) {
