b32fef97d7165f8c1f78539d3b6898d47d9438e5
jahangir-1995 <jahangir199511@gmail.com>
2022-01-30 07:43:02 +0600

    inch even-odd factorialWithFunction Add

--- factorial.js
+++ factorial.js
@@ -2,4 +2,17 @@ var factorial = 1;
  for (var i =1; i <= 7; i++) {
      factorial = factorial * i;
  }
- console.log(factorial);
+ console.log(factorial); //Out put= 5040;
+
+
+ // function diye
+ function getFacto(nums) {
+     let facto = 1;
+     for (let i = 1; i <= nums; i++){
+         facto = facto * i;
+     }
+     return facto;
+ }
+ var nums = 5;
+ var fstFacto = getFacto(nums);
+ console.log(fstFacto); //Out put=120;
