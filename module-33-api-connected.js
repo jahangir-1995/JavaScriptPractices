034706ac82baec048feb1e9a157971f70c7f579a
jahangir-1995 <jahangir199511@gmail.com>
2022-02-27 12:20:34 +0600

    json function with onclick

--- module-33-api-connected.js
+++ module-33-api-connected.js
@@ -10,11 +10,18 @@ function loadUsers(){
         .then(data => displayUser(data));
 }
 
+function displayUser(data){
+    const ul = document.getElementById("users")
+    for(const user of data){
+        const li = document.createElement("li");
+        // li.innerText = user.name;
+        li.innerText = `name: ${user.name} Email: ${user.email}`;
+        ul.appendChild(li);
+    }
+}
+
 function loadPost(){
     fetch("https://jsonplaceholder.typicode.com/posts")
         .then(res => res.json())
         .then(data => console.log(data));
-}
-function displayUser(data){
-    console.log(data)
 }
