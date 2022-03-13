b8de6aa90fe1b7231f79ab673a01e393ae12374e
jahangir-1995 <jahangir199511@gmail.com>
2022-03-13 08:06:19 +0600

    image folderr add

---
+++ my-slaider/my-slaider.js
@@ -0,0 +1,18 @@
+const images = [
+    "image/pic-1.jpg",
+    "image/pic-2.jpg",
+    "image/pic-3.jpg",
+    "image/pic-4.jpg",
+    "image/pic-5.jpg",
+    "image/pic-6.jpg",
+    "image/pic-7.jpg",
+    "image/pic-8.jpg",
+    "image/pic-9.jpg"
+]
+// image ready nai tai dekhabe  na
+
+let imgIndex = 0;
+
+setInterval(() => {
+    imgIndex++;
+}, 1000);
